43983d61c1aa199abcf46cb502b2a01d2f0b8d43
Nicolas Wagner <nicolas.wagner@epfl.ch>
2023-12-08 19:23:49 +0100

    cross val lambda

---
+++ cross_val_lambda_protonet.sh
@@ -0,0 +1,12 @@
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=0.1 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=1 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=5 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=10 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=20 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=50 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=100 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=1000 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=2000 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=5000 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot sot=true lambda_=10000 &&
+python run.py exp.name=cross_val_protonet_lambda method=protonet dataset=swiss_prot
