de3f0376b2def7846c91bf10b027b7cebf7cf2de
Nicolas Wagner <nicolas.wagner@epfl.ch>
2023-12-12 11:51:38 +0100

    change pretrained

--- run.py
+++ run.py
@@ -12,6 +12,21 @@ from utils.io_utils import get_resume_file, hydra_setup, fix_seed, model_to_dict
 
 
 def initialize_dataset_model(cfg):
+    if cfg.pretrained:
+        print('Using pretrained best backbone model')
+        pretrained_file = os.path.join(cfg.checkpoint.dir, 'pretrained_model.tar')
+        if os.path.isfile(pretrained_file):
+            model = torch.load(pretrained_file)['state']
+            print(model.keys())
+
+
+
+            assert False
+        else:
+            raise NameError(f'No pretrained model found at {pretrained_file}')
+
+
+
     # Instantiate train dataset as specified in dataset config under simple_cls or set_cls
     if cfg.method.type == "baseline":
         train_dataset = instantiate(cfg.dataset.simple_cls, batch_size=cfg.method.train_batch, mode='train')
@@ -57,7 +72,11 @@ def initialize_dataset_model(cfg):
         pretrained_file = os.path.join(cfg.checkpoint.dir, 'pretrained_model.tar')
         if os.path.isfile(pretrained_file):
             model = torch.load(pretrained_file)['state']
-            print(model)
+            print(model.keys())
+
+
+
+            assert False
         else:
             raise NameError(f'No pretrained model found at {pretrained_file}')
 
@@ -162,10 +181,10 @@ def train(train_loader, val_loader, model, cfg):
                 outfile = os.path.join(cp_dir, 'best_model.tar')
                 torch.save({'epoch': epoch, 'state': model.state_dict()}, outfile)
                 if not cfg.sot:
-                    print("save best pretrained model...")
-                    max_acc = acc
-                    outfile = os.path.join(cfg.checkpoint.dir, 'pretrained_model.tar')
-                    torch.save({'state': model.state_dict()}, outfile)
+                    pretrained_file = os.path.join(cfg.checkpoint.dir, 'pretrained_model.tar')
+                else:
+                    pretrained_file = os.path.join(cfg.checkpoint.dir, 'pretrained_sot_model.tar')
+                torch.save({'state': model.state_dict()}, pretrained_file)
 
         if epoch % cfg.exp.save_freq == 0 or epoch == cfg.method.stop_epoch - 1:
             outfile = os.path.join(cp_dir, '{:d}.tar'.format(epoch))
