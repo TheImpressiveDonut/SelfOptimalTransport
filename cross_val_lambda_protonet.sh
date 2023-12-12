a0e8195e7afe2d047242ffc5f117d7635273dced
Nicolas Wagner <nicolas.wagner@epfl.ch>
2023-12-12 13:37:40 +0100

    correct script

--- cross_val_lambda_protonet.sh
+++ cross_val_lambda_protonet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-lambdas=(5, 10, 20)
-n_iters=(10, 20, 50)
+lambdas=(5 10 20)
+n_iters=(10 20 50)
 method="maml"
 dataset="tabula_muris"
 
@@ -11,7 +11,7 @@ do
   do
     echo "Running with lambda $lambda and n_iter $n_iter"
     echo "-------------------------------------"
-    python run.py exp.name=CV_lambda_"$lambda"_niter_"$n_iter" method="$method" dataset="$dataset" sot=true lambda_="$lambda" n_iters="$n_iter"
+    python run.py exp.name=CV_"$method"_"$dataset" method="$method" dataset="$dataset" sot=true lambda_="$lambda" n_iters="$n_iter"
     echo "====================================="
   done
 done
