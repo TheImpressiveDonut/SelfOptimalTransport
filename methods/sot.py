6c3e2db6631beb676d188fb3346121aaa81f35e4
Delacourt <remi.delacourt@gmail.com>
2023-12-12 11:31:40 +0100

    Add sinkohrn-log conversion to normal space

--- methods/sot.py
+++ methods/sot.py
@@ -21,15 +21,16 @@ class Sot(nn.Module):
         D = 2 * (1 - S)
         # Set alpha_ in diagonal to act as infinite cost
         D.fill_diagonal_(self.alpha_)
-        # Compute Sinkhorn
+        # Compute Sinkhorn in log-space
         W = self.sinkhorn_log(D, self.lambda_, self.n_iter)
+        W = torch.exp(W)
         # Set 1 in diagonal (prob of similarity between x_i and x_i is 1)
         W.fill_diagonal_(1)
         return W
 
     def sinkhorn_log(self, D: Tensor, lambda_: float, num_iters: int) -> Tensor:
         log_k = -D * lambda_
-        log_v = torch.zeros(D.size()[1], device=D.device)  # constraint vector of all ones but to log so zeros
+        log_v = torch.zeros(D.size(1), device=D.device)  # constraint vector of all ones but to log so zeros
 
         for i in range(num_iters):
             log_u = -torch.logsumexp(log_k + log_v.unsqueeze(0), dim=1)
