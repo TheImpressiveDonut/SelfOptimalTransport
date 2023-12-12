e175f96d992bb90e3f9ebc981a2e6549833cfc54
Nicolas Wagner <nicolas.wagner@epfl.ch>
2023-12-12 22:41:48 +0100

    cross_val all methods

--- cross_val_lambda_niters.sh
+++ cross_val_lambda_niters.sh
@@ -2,8 +2,39 @@
 
 lambdas=(5 10 20)
 n_iters=(10 20 50)
-method="maml"
 dataset="tabula_muris"
+
+method="maml"
+
+for n_iter in "${n_iters[@]}"
+do
+  for lambda in "${lambdas[@]}"
+  do
+    echo "Running with lambda $lambda and n_iter $n_iter"
+    echo "-------------------------------------"
+    python run.py exp.name=CV_"$method"_"$dataset" method="$method" dataset="$dataset" sot=true lambda_="$lambda" n_iters="$n_iter"
+    echo "====================================="
+  done
+done
+
+python run.py exp.name=CV_"$method"_"$dataset" method="$method" dataset="$dataset"
+
+method="protonet"
+
+for n_iter in "${n_iters[@]}"
+do
+  for lambda in "${lambdas[@]}"
+  do
+    echo "Running with lambda $lambda and n_iter $n_iter"
+    echo "-------------------------------------"
+    python run.py exp.name=CV_"$method"_"$dataset" method="$method" dataset="$dataset" sot=true lambda_="$lambda" n_iters="$n_iter"
+    echo "====================================="
+  done
+done
+
+python run.py exp.name=CV_"$method"_"$dataset" method="$method" dataset="$dataset"
+
+method="matchingnet"
 
 for n_iter in "${n_iters[@]}"
 do
