3080602d87aad8a04128bc6968d9b768be962508
Delacourt <remi.delacourt@gmail.com>
2023-12-12 16:25:21 +0100

    Move config in run

--- run.py
+++ run.py
@@ -36,11 +36,6 @@ def initialize_dataset_model(cfg):
     else:
         backbone = instantiate(cfg.backbone, x_dim=train_dataset.dim)
 
-    if cfg.method.name == "maml" and cfg.dataset.set_cls.n_support != cfg.dataset.set_cls.n_query:
-        print("===>PERSONAL WARNING: For MAML with Sot, n_support and n_query should be equal (n_support <=> n_shot). Since in the config they are not equal, we set n_query = n_support. We also do this when Sot isn't used, to compare apples with apples.")
-        cfg.dataset.set_cls.n_query = cfg.dataset.set_cls.n_support
-        cfg.n_query = cfg.n_shot
-
     # Instantiate sot if necessary
     sot = None
     if cfg.sot:
@@ -98,6 +93,11 @@ def run(cfg):
     if ((cfg.method.name == "baseline" or cfg.method.name == "baseline_pp")
             and cfg.dataset.name == "swissprot"):
         cfg.train_classes = 7195  # tweak for baseline so that it can run correctly
+
+    if cfg.method.name == "maml" and cfg.dataset.set_cls.n_support != cfg.dataset.set_cls.n_query:
+        print("===>PERSONAL WARNING: For MAML with SOT, n_support and n_query should be equal (n_support <=> n_shot). Since in the config they are not equal, we set n_query = n_support. We also do this when Sot isn't used, to compare apples with apples.")
+        cfg.dataset.set_cls.n_query = cfg.dataset.set_cls.n_support
+        cfg.n_query = cfg.n_shot
 
     print(OmegaConf.to_yaml(cfg, resolve=True))
 
