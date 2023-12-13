35162a2d4f29ef9b8d64c96d3b5f7d3370d91f4a
Nicolas Wagner <nicolas.wagner@epfl.ch>
2023-12-13 10:03:24 +0100

    add episode

--- cross_val_lambda_niters.sh
+++ cross_val_lambda_niters.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-lambdas=(5 10)
+lambdas=(10)
 n_iters=(10)
 lrs=(0.1 0.01 0.001)
+n_episodes=(100 200 300)
 dataset="tabula_muris"
 method="protonet"
 
@@ -12,10 +13,13 @@ do
   do
     for lr in "${lrs[@]}"
     do
-      echo "Running with lambda $lambda, n_iter $n_iter and lr $lr"
-      echo "-------------------------------------"
-      python run.py exp.name=CV_"$method"_"$dataset"_lr method="$method" dataset="$dataset" sot=true lambda_="$lambda" n_iters="$n_iter" lr="$lr"
-      echo "====================================="
+      for n_episode in "${n_episodes[@]}"
+      do
+        echo "Running with lambda $lambda, n_iter $n_iter, n_episode $n_episode and lr $lr"
+        echo "-------------------------------------"
+        python run.py exp.name=CV_"$method"_"$dataset"_lr method="$method" dataset="$dataset" sot=true lambda_="$lambda" n_iters="$n_iter" lr="$lr" n_episode="$n_episode"
+        echo "====================================="
+      done
     done
   done
 done
